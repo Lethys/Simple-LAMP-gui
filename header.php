0f1baf22687d2cbd34e321760f743fe877c46928
Baptiste Caquot <Lethys89@gmail.com>
2011-02-21 11:43:21 +0100

    tout

--- header.php
+++ header.php
@@ -8,6 +8,15 @@
 		 <style type="text/css">
 		  	@import url(css/style.css);
 		</style>
+		<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4/jquery.min.js"></script>
+		<script type="text/javascript" src="./js/livefilter/jquery.liveFilter.js"></script>
+		<script type="text/javascript">
+			$(document).ready(function() {
+			    $('#filter').liveFilter('ul');
+			});
+		</script>
+
+
 	</head> 
 	
 <body>
--- index.php
+++ index.php
@@ -5,12 +5,18 @@
 <?php
 $rep = "./";
 $dir = opendir($rep); 
+echo '<div id="filter">';
+echo '<input type="text" class="filter" name="liveFilter" />';
 echo '<ul>';
 while ($f = readdir($dir)) {
    if(is_dir($rep.$f)) {
-      echo '<li><a href="'.$rep.$f.'"class="fontface"> '.$f.'</a></li>';
+      echo '<li>
+      				<a href="'.$rep.$f.'"class="fontface"><img src="'.$rep.$f.'/image-site.png" alt="'.$f.'" height="50px" width="50px"/></a>
+      				<a href="'.$rep.$f.'"class="fontface"> '.$f.'</a>
+       			</li>';
    }
 }
 echo '</ul>';
+echo '</div>';
 closedir($dir)
 ?>
