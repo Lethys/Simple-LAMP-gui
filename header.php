c03c00bd1afc85f7b029746bc25430c77f4e91ca
Baptiste Caquot <Lethys89@gmail.com>
2011-02-21 10:43:15 +0100

    css

---
+++ header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html> 
+<html lang="fr"> 
+	 <head> 
+		 <meta charset="utf-8"> 
+		 <meta http-equiv="Content-type" content="text/html;charset=UTF-8" /> 
+		 <title>Simple LAMP gui</title> 
+		 <meta name="author" content="Baptiste Caquot" /> 
+		 <style type="text/css">
+		  	@import url(css/style.css);
+		</style>
+	</head> 
+	
+<body>
--- index.php
+++ index.php
@@ -1,11 +1,16 @@
+<?php 
+	include 'header.php';
+?>
+
 <?php
 $rep = "./";
 $dir = opendir($rep); 
-
+echo '<ul>';
 while ($f = readdir($dir)) {
    if(is_dir($rep.$f)) {
-      echo '<li><a href="'.$rep.$f.'"> '.$f.'</a></li>';
+      echo '<li><a href="'.$rep.$f.'"class="fontface"> '.$f.'</a></li>';
    }
 }
+echo '</ul>';
 closedir($dir)
 ?>
