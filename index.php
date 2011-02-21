5752b7546fe2f89cccab99037ed32d9c74dc98ea
Baptiste Caquot <Lethys89@gmail.com>
2011-02-21 09:25:11 +0100

    test

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
 <?php
-    echo "";
+$rep = "./";
+$dir = opendir($rep); 
+
+while ($f = readdir($dir)) {
+   if(is_dir($rep.$f)) {
+      echo '<li><a href="'.$rep.$f.'"> '.$f.'</a></li>';
+   }
+}
+closedir($dir)
 ?>
